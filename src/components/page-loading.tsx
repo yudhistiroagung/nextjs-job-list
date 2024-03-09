d760d1a7e14172dd1ca24f67cb952bfe065602f3
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2024-03-09 19:53:58 +0700

    fix loading position

--- src/components/page-loading.tsx
+++ src/components/page-loading.tsx
@@ -1,6 +1,6 @@
 export function PageLoading() {
   return (
-    <div className="m-24">
+    <div className="flex flex-1 m-24 w-full items-center justify-center">
       <span className="loading loading-spinner loading-lg"></span>
     </div>
   );
